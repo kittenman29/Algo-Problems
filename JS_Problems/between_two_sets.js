4f0ad3382a8a69c2bffdec532fb6b970ab5c3dbf
Will Schulz <will.schulz29@gmail.com>
2020-01-16 18:01:28 -0400

    between two sets

---
+++ JS_Problems/between_two_sets.js
@@ -0,0 +1,63 @@
+// You will be given two arrays of integers and asked to determine all integers that satisfy the following two conditions:
+
+// The elements of the first array are all factors of the integer being considered
+// The integer being considered is a factor of all elements of the second array
+// These numbers are referred to as being between the two arrays. You must determine how many such numbers exist.
+
+// For example, given the arrays  and , there are two numbers between them:  and . , ,  and  for the first value. Similarly, ,  and , .
+
+// Function Description
+
+// Complete the getTotalX function in the editor below. It should return the number of integers that are betwen the sets.
+
+// getTotalX has the following parameter(s):
+
+// a: an array of integers
+// b: an array of integers
+// Input Format
+
+// The first line contains two space-separated integers,  and , the number of elements in array  and the number of elements in array .
+// The second line contains  distinct space-separated integers describing  where .
+// The third line contains  distinct space-separated integers describing  where .
+
+// Constraints
+
+// Output Format
+
+// Print the number of integers that are considered to be between  and .
+
+// Sample Input
+
+// 2 3
+// 2 4
+// 16 32 96
+// Sample Output
+
+// 3
+// Explanation
+
+// 2 and 4 divide evenly into 4, 8, 12 and 16.
+// 4, 8 and 16 divide evenly into 16, 32, 96.
+
+// 4, 8 and 16 are the only three numbers for which each element of a is a factor and each is a factor of all elements of b.
+
+function getTotalX(a, b) {
+  // Write your code here
+  let total = 0;
+  let maxA = Math.max(...a);
+  let minB = Math.min(...b);
+  let number = maxA;
+
+  let allElementsAreMultiple = false;
+  let numberIsMultipleOfAll = false;
+
+  while (number <= minB) {
+    console.log("aMax ", maxA, "bMin ", minB, "number ", number);
+    allElementsAreMultiple = a.every(ele => number % ele === 0);
+    numberIsMultipleOfAll = b.every(ele => ele % number === 0);
+    if (allElementsAreMultiple && numberIsMultipleOfAll) total++;
+    number++;
+  }
+
+  return total;
+}
