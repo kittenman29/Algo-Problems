6523d01a86cf261b740cdc10f5f0543ff98051c1
Will Schulz <will.schulz29@gmail.com>
2020-01-20 16:09:48 -0400

    init and start sql problems

---
+++ SQL_Problems/revising_the_select.sql
@@ -0,0 +1,12 @@
+-- Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.
+
+-- Input Format
+
+-- The CITY table is described as follows: 
+
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
